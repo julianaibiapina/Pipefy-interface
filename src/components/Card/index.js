46b99f0672b7af34743d1622eabd082336f715d5
julianaibiapina <julianibiapina@alu.ufc.br>
2020-07-07 18:15:54 -0300

    conclusão v1

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,16 +1,68 @@
-import React from 'react';
+import React, { useRef, useContext } from 'react';
+import BoardContext from '../Board/context';
 
+
+import { useDrag, useDrop } from 'react-dnd';
+ 
 import { Container, Label } from './styles';
 
-export default function Card(){
+export default function Card({data, index, listIndex}){
+    const ref = useRef();
+    const { move } = useContext(BoardContext);
+
+    const [{ isDragging }, dragRef] =  useDrag({
+        item: { type: 'CARD', index, listIndex},
+        collect: monitor => ({
+            isDragging: monitor.isDragging(),
+        }),
+    });
+
+    const [, dropRef] = useDrop({
+        accept: 'CARD',
+        hover(item, monitor){
+            const draggeListIndex =  item.listIndex
+            const targetListIndex =  listIndex;
+            const draggedIndex = item.index;
+            const targetIndex = index;
+            
+            if(draggedIndex === targetIndex && draggeListIndex === targetListIndex){
+                return;
+            }
+
+
+            const targetSize = ref.current.getBoundingClientRect();
+            const targetCenter = (targetSize.bottom - targetSize.top) / 2;
+            
+            
+            const draggedOffset = monitor.getClientOffset();
+            const draggedTop = draggedOffset.y - targetSize.top;
+
+            if(draggedIndex < targetIndex &&  draggedTop < targetCenter){
+                return;
+            }
+
+            if(draggedIndex > targetIndex &&  draggedTop > targetCenter){
+                return;
+            }
+            
+            move(draggeListIndex, targetListIndex, draggedIndex, targetIndex);
+
+            item.index = targetIndex;
+            item.listIndex = targetListIndex;
+            
+        }
+    });
+
+    dragRef(dropRef(ref));
+
     return (
-        <Container>
+        <Container ref={ref} isDragging={isDragging}>
             <header>
-                <Label color="#7159c1" />
+                {data.labels.map(label => <Label key={label} color={label}/>)}
             </header>
 
-            <p>Fazer migração completa de servidor</p>
-            <img src="https://api.adorable.io/avatars/285/abott@adorable.png" alt=""/>
+            <p>{data.content}</p>
+            {data.user && <img src={data.user} alt=""/>}
         </Container>
     );
 }
